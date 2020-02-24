a0339da6869643f94fb0119a8898c41c52fba2a0
Michael Quigley <mikequigley@quigleypublishing.com>
2020-02-24 23:31:05 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,4 +10,22 @@ class School
     @roster
   end
 
+  def add_student(name, grade)
+    if @roster[grade] != nil # don't forget this line (will get undefined method for nil class error)
+      @roster[grade] = << name 
+    else 
+      @roster[grade] = [name] # make sure to put name in brackets. Otherwise it will add to the key name
+    end  
+  end   
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort 
+    @roster.each do |key, value| 
+      value.sort!
+    end
+  end 
+
 end
